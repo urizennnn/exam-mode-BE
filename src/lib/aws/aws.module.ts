f0a8861de7886c115957ba51c82f9c4f6eebfe6b
urizennnn <igamerryt@gmail.com>
2025-06-10 18:50:54 +0100

    chore: added aws controller for file upload activities

--- src/lib/aws/aws.module.ts
+++ src/lib/aws/aws.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AwsService } from './aws.service';
 import { ConfigModule } from '@nestjs/config';
+import { AwsController } from './aws.controller';
 
 @Module({
   imports: [ConfigModule],
-  controllers: [],
+  controllers: [AwsController],
   providers: [AwsService],
   exports: [AwsService],
 })
